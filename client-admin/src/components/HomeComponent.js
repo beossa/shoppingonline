8af28d6ed5f93f117e88b115ad6af3961d86658f
beossa <94435373+beossa@users.noreply.github.com>
2024-07-16 08:02:41 +0700

    Add files via upload

---
+++ client-admin/src/components/HomeComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="align-center">
+        <h2 className="text-center">ADMIN HOME</h2>
+        <img src="https://www.troublefreepool.com/media/hello-gif.3474/full" width="800px" height="600px" alt="" />
+      </div>
+    );
+  }
+}
+export default Home;
---
+++ client-admin/src/contexts/MyContext.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import axios from 'axios';
+const MyContext = React.createContext();
+export default MyContext;
