52391b502936753d2224b63f4152780b23430852
beossa <94435373+beossa@users.noreply.github.com>
2024-07-16 07:54:18 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'shoppingonl.bdr3n9k.mongodb.net',
+    DB_USER: 'beobb',
+    DB_PASS: 'Minhphu0ng2304',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'beobugbu0107',
+    JWT_EXPIRES: '3600000', // in milliseconds
+    EMAIL_USER: 'phuong23042003@gmail.com', // gmail service
+    EMAIL_PASS: 'ialh ctbt hwxm uouy'
+  };
+  module.exports = MyConstants;
